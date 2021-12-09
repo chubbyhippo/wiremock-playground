53c98cf046f5c10ce25cbfd8f7bb8d2143b090a2
ch <mkmsacc@outlook.com>
2021-12-09 15:48:52 +0700

    Add uri

--- movies-app/src/main/java/com/example/constants/MoviesAppConstants.java
+++ movies-app/src/main/java/com/example/constants/MoviesAppConstants.java
@@ -6,4 +6,5 @@ public class MoviesAppConstants {
     }
 
     public static final String GET_ALL_MOVIES_V1 = "/movieservice/v1/allMovies";
+    public static final String RETRIEVE_MOVIE_BY_ID = "/movieservice/v1/movie/{id}";
 }
