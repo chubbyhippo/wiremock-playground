af203fde717fc3882c8085dc00505f48325daabb
ch <mkmsacc@outlook.com>
2021-12-09 14:53:07 +0700

    Add Movie dto

---
+++ movies-app/src/main/java/com/example/dto/Movie.java
@@ -0,0 +1,22 @@
+package com.example.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie {
+    @JsonProperty("movie_id")
+    private Long movieId;
+    private String cast;
+    private String name;
+    @JsonProperty("release_date")
+    private LocalDate releaseDate;
+    private Integer year;
+
+}
