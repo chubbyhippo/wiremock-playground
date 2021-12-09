8788657c60ba84759d9d161bdeac3796f339af11
ch <mkmsacc@outlook.com>
2021-12-09 14:58:33 +0700

    Add constant uri

---
+++ movies-app/src/main/java/com/example/constants/MoviesAppConstants.java
@@ -0,0 +1,9 @@
+package com.example.constants;
+
+public class MoviesAppConstants {
+    private MoviesAppConstants() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static final String GET_ALL_MOVIES_V1 = "/movieservice/v1/allMovies";
+}
