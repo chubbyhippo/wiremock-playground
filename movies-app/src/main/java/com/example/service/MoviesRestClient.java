52b5642eef2163cf1b3c69e9962e1c15ad86fe9a
ch <mkmsacc@outlook.com>
2021-12-09 15:48:23 +0700

    Add retrieveMovieById

--- movies-app/src/main/java/com/example/service/MoviesRestClient.java
+++ movies-app/src/main/java/com/example/service/MoviesRestClient.java
@@ -2,12 +2,16 @@ package com.example.service;
 
 import com.example.constants.MoviesAppConstants;
 import com.example.dto.Movie;
+import com.example.exception.MovieErrorResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.util.List;
 
 @RequiredArgsConstructor
+@Slf4j
 public class MoviesRestClient {
     private final WebClient webClient;
 
@@ -17,5 +21,22 @@ public class MoviesRestClient {
                 .bodyToFlux(Movie.class)
                 .collectList()
                 .block();
+    }
+
+    public Movie retrieveMovieById(Integer movieId) {
+        try {
+            return webClient.get().uri(MoviesAppConstants.RETRIEVE_MOVIE_BY_ID, movieId)
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
+        } catch (WebClientResponseException e) {
+            log.error("WebClientResponseException in retrieveMovieById. Status code is {} and the message is {} ",
+                    e.getRawStatusCode(),
+                    e.getResponseBodyAsString());
+            throw new MovieErrorResponse(e.getStatusText(), e);
+        } catch (Exception e) {
+            log.error("Exception in retrieveMovieById and the message is {} ", e.getMessage());
+            throw new MovieErrorResponse(e);
+        }
     }
 }
