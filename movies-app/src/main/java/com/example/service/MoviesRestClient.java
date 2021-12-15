3fe12658e7ac68fcdc6f86e0531b525febb1a88f
ch <mkmsacc@outlook.com>
2021-12-15 07:58:35 +0700

    Add retrieveMoviesByName

--- movies-app/src/main/java/com/example/service/MoviesRestClient.java
+++ movies-app/src/main/java/com/example/service/MoviesRestClient.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -28,6 +29,30 @@ public class MoviesRestClient {
             return webClient.get().uri(MoviesAppConstants.RETRIEVE_MOVIE_BY_ID, movieId)
                     .retrieve()
                     .bodyToMono(Movie.class)
+                    .block();
+        } catch (WebClientResponseException e) {
+            log.error("WebClientResponseException in retrieveMovieById. Status code is {} and the message is {} ",
+                    e.getRawStatusCode(),
+                    e.getResponseBodyAsString());
+            throw new MovieErrorResponse(e.getStatusText(), e);
+        } catch (Exception e) {
+            log.error("Exception in retrieveMovieById and the message is {} ", e.getMessage());
+            throw new MovieErrorResponse(e);
+        }
+    }
+
+    public List<Movie> retrieveMoviesByName(String name) {
+        String retrieveByNameUri = UriComponentsBuilder.fromUriString(MoviesAppConstants.MOVIE_BY_NAME_QUERY_PARAM_V1)
+                .queryParam("movie_name", name)
+                .buildAndExpand()
+                .toUriString();
+
+        try {
+            return webClient.get()
+                    .uri(retrieveByNameUri)
+                    .retrieve()
+                    .bodyToFlux(Movie.class)
+                    .collectList()
                     .block();
         } catch (WebClientResponseException e) {
             log.error("WebClientResponseException in retrieveMovieById. Status code is {} and the message is {} ",
