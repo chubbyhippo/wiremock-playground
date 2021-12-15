faafb96afc783cce2f0c9e2905353e538a0a88c2
ch <mkmsacc@outlook.com>
2021-12-15 07:58:18 +0700

    Add more constants

--- movies-app/src/main/java/com/example/constants/MoviesAppConstants.java
+++ movies-app/src/main/java/com/example/constants/MoviesAppConstants.java
@@ -7,4 +7,5 @@ public class MoviesAppConstants {
 
     public static final String GET_ALL_MOVIES_V1 = "/movieservice/v1/allMovies";
     public static final String RETRIEVE_MOVIE_BY_ID = "/movieservice/v1/movie/{id}";
+    public static final String MOVIE_BY_NAME_QUERY_PARAM_V1 = "movieservice/v1/movieName";
 }
