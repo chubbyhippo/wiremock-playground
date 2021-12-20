3bacda1c29fc4a112817564892d8ba77a9bfd4fc
ch <mkmsacc@outlook.com>
2021-12-20 08:49:32 +0700

    Update id data type to Integer

--- movies-app/src/main/java/com/example/dto/Movie.java
+++ movies-app/src/main/java/com/example/dto/Movie.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 @Builder
 public class Movie {
     @JsonProperty("movie_id")
-    private Long movieId;
+    private Integer movieId;
     private String cast;
     private String name;
     @JsonProperty("release_date")
