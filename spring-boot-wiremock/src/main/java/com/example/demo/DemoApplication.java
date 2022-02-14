70d41138ced2af83ea3884caeb6c5514b1fcf54f
ch <mkmsacc@outlook.com>
2022-02-14 08:43:46 +0700

    Add WebClient bean

---
+++ spring-boot-wiremock/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Value("${movieapp.baseUrl}")
+    private String baseUrl;
+
+    @Bean
+    public WebClient webClient() {
+        var httpClient = HttpClient.create()
+                .doOnConnected(connection -> connection
+                        .addHandler(new ReadTimeoutHandler(5))
+                        .addHandler(new WriteTimeoutHandler(5)));
+        return WebClient.builder()
+                .baseUrl(baseUrl)
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+    }
+
+}
