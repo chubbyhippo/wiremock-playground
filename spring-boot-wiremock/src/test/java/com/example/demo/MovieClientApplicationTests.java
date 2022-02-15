f05367a872f8d3a96a58b7d8df5bd9f3027e3153
ch <mkmsacc@outlook.com>
2022-02-15 08:00:52 +0700

    Remove public

--- spring-boot-wiremock/src/test/java/com/example/demo/MovieClientApplicationTests.java
+++ spring-boot-wiremock/src/test/java/com/example/demo/MovieClientApplicationTests.java
@@ -13,7 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWireMock(port = 0)
-public class MovieClientApplicationTests {
+class MovieClientApplicationTests {
     @Autowired
     MoviesRestClient moviesRestClient;
 
