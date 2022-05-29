18dca8d1a6b131d0913aaeb8d83e92e53a6bc4a7
ch <mkmsacc@outlook.com>
2022-05-29 16:02:16 +0700

    Update TestUtil

--- reactive-movies-restful-api/src/test/kotlin/com/movies/util/TestUtil.kt
+++ reactive-movies-restful-api/src/test/kotlin/com/movies/util/TestUtil.kt
@@ -5,5 +5,8 @@ import com.movies.domain.Review
 import java.time.LocalDate
 
 fun getMovieInfoK() = MovieInfo(null, "The Dark Knight", 2008, null, LocalDate.parse("2008-07-18"))
-fun getMovieInfoKWithCast(movieInfoId : Long?=null) = MovieInfo(movieInfoId, "The Dark Knight", 2008, mutableListOf("Christian Bale","Tom Hardy"), LocalDate.parse("2008-07-18"))
-fun getReview(movieInfo: MovieInfo, rating: Double?  = 8.9) = Review(null,movieInfo.movieInfoId!!,rating, "Awesome Movie")
+fun getMovieInfoKWithCast(movieInfoId: Long? = null) =
+    MovieInfo(movieInfoId, "The Dark Knight", 2008, "Christian Bale", LocalDate.parse("2008-07-18"))
+
+fun getReview(movieInfo: MovieInfo, rating: Double? = 8.9) =
+    Review(null, movieInfo.movieInfoId!!, rating, "Awesome Movie")
